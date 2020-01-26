680f956004e0f0596082bb1d324563f2d793c05f
TechnoStudy <info@techno.study>
2020-01-26 11:51:50 -0500

    index based reading

--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -4,12 +4,14 @@ import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Iterator;
 
 public class TestCases {
     private static final String FILE_NAME = "src/test/resources/Books.xlsx";
 
+    // this method ignores blanks
     @Test
     public void test1() throws IOException {
         FileInputStream excelFile = new FileInputStream(new File(FILE_NAME));
@@ -32,4 +34,28 @@ public class TestCases {
             System.out.println();
         }
     }
+
+    //this method reads all values
+    @Test
+    public void test2() throws IOException {
+        FileInputStream excelFile = new FileInputStream(new File(FILE_NAME));
+        Workbook workbook = new XSSFWorkbook(excelFile);
+        Sheet datatypeSheet = workbook.getSheetAt(0);
+        int lastRowNum = datatypeSheet.getLastRowNum();
+        int firstRowNum = datatypeSheet.getFirstRowNum();
+        int rowCount = lastRowNum - firstRowNum;
+        int cellCount = datatypeSheet.getRow(1).getLastCellNum() - datatypeSheet.getRow(1).getFirstCellNum();
+
+        for (int i = 0; i < rowCount; i++) {
+            Row row = datatypeSheet.getRow(i);
+            if(row != null) {
+                for (int j = 0; j < cellCount; j++) {
+                    if(row.getCell(j) != null)
+                        System.out.print(row.getCell(j).toString() + " --- ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
 }
